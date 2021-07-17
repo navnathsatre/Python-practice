c305cc6e898e97b52ae07b169de55739a4b915e4
NAVNATH SATRE <75266852+navnathsatre@users.noreply.github.com>
2021-07-17 21:59:07 +0530

    Add files via upload

---
+++ Bmw.py
@@ -0,0 +1,6 @@
+# Python code to illustrate the Modules
+def Bmw():
+	models = ['i8', 'x1', 'x5', 'x6']
+
+	# A normal print function
+	print(models)
---
+++ sample.py
@@ -0,0 +1,22 @@
+# Import classes from your brand new package
+from Cars import Bmw
+from Cars import Audi
+from Cars import Nissan
+#from Bmw import Bmw
+#from Audi import Audi
+#from Nissan import Nissan
+
+# Create an object of Bmw class & call its method
+ModBMW = Bmw()
+ModBMW.outModels()
+
+# Create an object of Audi class & call its method
+ModAudi = Audi()
+ModAudi.outModels()
+
+# Create an object of Nissan class & call its method
+ModNissan = Nissan()
+ModNissan.outModels()
+
+
+
